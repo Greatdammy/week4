ca99883027303d20fb17e96cc818c34ce882b6b0
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-04-20 17:58:37 +0100

    Add project files.

---
+++ week4.academy/Figure.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace week4.academy
+{
+    public class Figure
+    {
+        public int X { get; set; }
+
+        public static Figure operator +(Figure left, Figure right)
+        {
+            var f = new Figure()
+            {
+                X = left.X + right.X,
+            };
+            return f;
+        }
+
+        public static Figure operator -(Figure left, Figure right)
+        {
+            var f = new Figure()
+            {
+                X = left.X - right.X,
+            };
+            return f;
+        }
+
+        public static Figure operator ++(Figure left)
+        {
+            var f = new Figure()
+            {
+                X = left.X + 1
+            };
+            return f;
+        }
+
+        public static bool operator ==(Figure left, Figure right)
+        {
+            return left.X == right.X;
+        }
+        public static bool operator !=(Figure left, Figure right)
+        {
+            return left.X != right.X;
+        }
+
+        public static bool operator <=(Figure left, Figure right)
+        {
+            return left.X <= right.X;
+        }
+
+        public static bool operator >=(Figure left, Figure right)
+        {              
+            return left.X >= right.X;
+        }
+    }
+    }
+
+      
---
+++ week4.academy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace week4.academy
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            //int i = 9;
+            //int j = 10;
+            //int result = i + j;
+
+            Figure o = new Figure() { X = 100 };
+
+            Figure o2 = new Figure() { X = 123 };
+            //Figure plus = o == o2;
+            bool isEqual = o == o2;
+            bool isEqual2 = o2 >= o;
+            //Figure o3 = new Figure() { X = 10 };
+
+            //Figure o4 = new Figure() { X = 20 };
+
+            //Figure plus = o + (o2 + o3) - o4;
+            //plus++;
+            //bool isEqual = o == o2;
+            Console.WriteLine(isEqual2);
+
+        }
+    }
+}
